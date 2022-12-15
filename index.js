64fac32cf5336b40e6187ec5aeaf449bc6fba537
Chantell Davis <chantelldavis2014@yahoo.com>
2022-12-15 14:13:30 -0500

    commit tree 2cd53c518b3399be5fb7b02385628684576a3184

---
+++ index.js
@@ -0,0 +1,26 @@
+// const divide = function(num1,num2){
+//    return(num1 / num2)
+// }
+// divide(2000 / 100)
+
+// const square = (a) => (a * a)
+
+// const add = (b,r) => (b + r)
+
+const divide = function(a,b){
+   return (a / b);
+}
+
+const square = (a) => (a * a);
+
+
+const add = (c,d) => (c + d);
+
+
+
+
+
+
+
+
+
